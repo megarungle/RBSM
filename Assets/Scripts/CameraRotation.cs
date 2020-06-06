f88dc02a1fc21aa20e13564c8b132547b95a5e4e
megarungle <megarungle@gmail.com>
2020-06-07 01:40:26 +0300

    Add camera motion

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        mouseXAndY = 0,
+        mouseX = 1,
+        mouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.mouseXAndY;
+
+    public float sensHor = 9.0f;
+    public float sensVert = 9.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (axes == RotationAxes.mouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensVert, 0);
+        }
+        else if (axes == RotationAxes.mouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensVert;
+            _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+}
