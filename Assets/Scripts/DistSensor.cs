f7821ab4b4b3247174067f4077024bd386266c52
Anton <da394372@gmail.com>
2020-06-09 21:15:33 +0300

    Added distanse sensor

---
+++ Assets/Scripts/DistSensor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistSensor : MonoBehaviour
+{
+    public float dist;
+    public int rayLen;
+    void Start() {
+        rayLen = 100;
+    }
+
+    void Update() {
+            float cX = this.transform.position.x;
+            float cY = this.transform.position.y - this.transform.localScale.y / 2;
+            float cZ = this.transform.position.z;
+
+            Vector3 centre = new Vector3(cX, cY, cZ); // Center of bottom edge of cube;
+            Vector3 end = this.transform.TransformDirection(-Vector3.up) * rayLen; // End point
+
+            Debug.DrawRay(centre, end, Color.green);
+
+            Ray r = new Ray(centre, end);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(r, out hit, rayLen))
+            {
+                dist = hit.distance;
+            }
+        }
+}
