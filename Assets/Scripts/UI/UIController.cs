2420722a7351af8a7f6e7bee0197757fbcaa5fc3
megarungle <megarungle@gmail.com>
2021-05-17 08:33:37 +0300

    RobotConstructor: UI part 1

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class UIController : MonoBehaviour
+{
+    // For mouse click imitation
+    [DllImport("user32.dll")]
+    private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+    private bool IsCollapsed = false;
+
+    public void BtnClick(GameObject btn)
+    {
+        switch (btn.name)
+        {
+            case "BtnCollapse":
+                RectTransform rt = gameObject.GetComponent(typeof(RectTransform)) as RectTransform;
+                rt.position = new Vector2(-rt.position.x, rt.position.y);
+                Animator anim = btn.GetComponent(typeof(Animator)) as Animator;
+                if (IsCollapsed)
+                {
+                    anim.SetInteger("UIInterrupt", 1);
+                    mouse_event((int)0x00000008, 0, 0, 0, 0); // right down at (0, 0)
+                    mouse_event((int)0x00000010, 0, 0, 0, 0); // right up at (0, 0)
+                }
+                IsCollapsed = !IsCollapsed;
+                break;
+            default:
+                break;
+        }
+    }
+}
