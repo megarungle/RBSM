ebdb4a64ec6125ee42481ed42e5c1d7eba5d4546
megarungle <megarungle@gmail.com>
2021-06-04 08:50:43 +0300

    MainMenu: part final

---
+++ Assets/Scripts/Utils/BackToMainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Threading;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    public ScreenFader screenFader;
+
+    private IEnumerator ChangeScene()
+    {
+        screenFader.fadeState = ScreenFader.FadeState.In;
+
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(ChangeScene());
+        }
+    }
+}
