dd0f251c641508eea6c2cb6118147cbc6b087e33
megarungle <megarungle@gmail.com>
2021-06-14 22:33:47 +0300

    Simulator: part 2

---
+++ Assets/Scripts/Simulator/Paths.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paths : MonoBehaviour
+{
+    public string robotPath = null;
+    public string scriptPath = null;
+    public string envPath = null;
+}
