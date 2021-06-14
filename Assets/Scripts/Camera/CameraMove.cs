0f6309ca9b89febb4a7890078f776e83ffba9b77
megarungle <megarungle@gmail.com>
2021-06-14 22:18:24 +0300

    Simulation init

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public int speed = 10;
+
+	void FixedUpdate () {
+		speed = 10;
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			speed = 3;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			gameObject.transform.position -= gameObject.transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			gameObject.transform.position -= gameObject.transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			gameObject.transform.position += gameObject.transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.Space))
+        {
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+				gameObject.transform.position.y + speed * Time.deltaTime,
+				gameObject.transform.position.z);
+		}
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+				gameObject.transform.position.y - 2 * speed * Time.deltaTime,
+				gameObject.transform.position.z);
+		}
+	}
+}
